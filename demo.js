36397f06d6b66e7803da0dd9016bb4930b52c6d4
Tom Février <tomfevrier@gmail.com>
2020-07-03 11:46:09 +0200

    Updating README

--- demo.js
+++ demo.js
@@ -19,41 +19,41 @@ const h2g2Characters = kw.DataFrame([
 	}
 ]);
 
-// // ... or load a CSV file into a DataFrame
-// const data = kw.loadCSV('sentiment_differences.csv');
-//
-// // Display DataFrames
-// h2g2Characters.show();	// Same as running console.log(h2g2Characters.toString())
-// data.show();			// Large cells and DataFrames get truncated
-//
-// // Access rows
-// console.log(h2g2Characters.first(), h2g2Characters.get(1));
-// console.log();
-//
-// h2g2Characters.name.show()
-//
-// // Show columns as Series
-// data['url'].show();
-// data.title.show();
-//
-// // Convert a DataFrame to an array of Objects
-// console.log(h2g2Characters.toArray());
-// console.log();
-//
-// // Display a slice of a DataFrame
-// data.slice(10, 24).show();
-//
-// // Iterate over the rows and indexes of a DataFrame
-// for (let [index, row] of h2g2Characters.items()) {
-// 	console.log(`${index + 1}.`);
-// 	console.log(`Name: ${row.name} ${row.surname}`);
-// 	console.log(`Occupation: ${row.occupation || 'N/A'}`);
-// 	console.log();
-// }
-//
-// // Drop N/A values
-// h2g2Characters.dropNA().show();
-// h2g2Characters.dropNA({ keep: [''], axis: 'columns' }).show();
+// ... or load a CSV file into a DataFrame
+const data = kw.loadCSV('sentiment_differences.csv');
+
+// Display DataFrames
+h2g2Characters.show();	// Same as running console.log(h2g2Characters.toString())
+data.show();			// Large cells and DataFrames get truncated
+
+// Access rows
+console.log(h2g2Characters.first(), h2g2Characters.get(1));
+console.log();
+
+h2g2Characters.name.show()
+
+// Show columns as Series
+data['url'].show();
+data.title.show();
+
+// Convert a DataFrame to an array of Objects
+console.log(h2g2Characters.toArray());
+console.log();
+
+// Display a slice of a DataFrame
+data.slice(10, 24).show();
+
+// Iterate over the rows and indexes of a DataFrame
+for (let [index, row] of h2g2Characters.items()) {
+	console.log(`${index + 1}.`);
+	console.log(`Name: ${row.name} ${row.surname}`);
+	console.log(`Occupation: ${row.occupation || 'N/A'}`);
+	console.log();
+}
+
+// Drop N/A values
+h2g2Characters.dropNA().show();
+h2g2Characters.dropNA({ keep: [''], axis: 'columns' }).show();
 
 // Append new rows to a DataFrame...
 h2g2Characters.append([
@@ -80,35 +80,35 @@ h2g2Characters.addColumn('fullName', h2g2Characters.map(e => [e.name, e.surname]
 
 h2g2Characters.dropNA().show();
 h2g2Characters.dropNA({ keep: [''], axis: 'columns' }).show();
-//
-// // Rename and reorder columns
-// h2g2Characters
-// 	.rename({ surname: 'familyName', occupation: 'job' })
-// 	.reorder(['familyName', 'name', 'species', 'job'])
-// 	.show();
-//
-// // Rename and reorder columns
-// h2g2Characters
-// 	.replace('Dent', 'Accroc', { inPlace: false, columns: ['occupation', 'name'] })
-// 	.show();
-//
-// h2g2Characters.concat(h2g2Characters).show();
-//
-//
-// // Add a new column to a DataFrame by applying a function to it, and save it as CSV
-// h2g2Characters.addColumn('fullName', h2g2Characters.map(e => `${e.name} ${e.surname}`), { inPlace: true });
-// h2g2Characters.show();
-// h2g2Characters.toCSV('h2g2Characters.csv');
-//
-// // Create a PivotTable on specific columns...
-// const pivotTable = data.pivot(['sector', 'date']);
-// pivotTable.show();
-//
-// // ...and apply a rollup function on this table
-// pivotTable.mean('score').show();
-//
-// // Sort articles by date and by score, and only show a selection of columns
-// data.sort(['date', 'score']).filter(['title', 'date', 'score']).show();
-//
-// // Filter articles published on 4/8/2020 and calculate their mean score after rounding them to 2 digits
-// console.log(data.filter(e => e.date == '2020-04-08').score.round(2).mean());
+
+// Rename and reorder columns
+h2g2Characters
+	.rename({ surname: 'familyName', occupation: 'job' })
+	.reorder(['familyName', 'name', 'species', 'job'])
+	.show();
+
+// Rename and reorder columns
+h2g2Characters
+	.replace('Dent', 'Accroc', { inPlace: false, columns: ['occupation', 'name'] })
+	.show();
+
+h2g2Characters.concat(h2g2Characters).show();
+
+
+// Add a new column to a DataFrame by applying a function to it, and save it as CSV
+h2g2Characters.addColumn('fullName', h2g2Characters.map(e => `${e.name} ${e.surname}`), { inPlace: true });
+h2g2Characters.show();
+h2g2Characters.toCSV('h2g2Characters.csv');
+
+// Create a PivotTable on specific columns...
+const pivotTable = data.pivot(['sector', 'date']);
+pivotTable.show();
+
+// ...and apply a rollup function on this table
+pivotTable.mean('score').show();
+
+// Sort articles by date and by score, and only show a selection of columns
+data.sort(['date', 'score']).filter(['title', 'date', 'score']).show();
+
+// Filter articles published on 4/8/2020 and calculate their mean score after rounding them to 2 digits
+console.log(data.filter(e => e.date == '2020-04-08').score.round(2).mean());
